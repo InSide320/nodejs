952cc59f7c3f55ca7e60fd47f1b0ed23ae049a27
Denys Kud <dekud2109@gmail.com>
2025-07-02 12:30:18 +0300

    Update server startup message to include the running URL

--- 07-mvc/implementation/03_mvc/bin/www.js
+++ 07-mvc/implementation/03_mvc/bin/www.js
@@ -2,4 +2,4 @@ const config = require('config');
 
 const PORT = config.get('port');
 const app = require('../app');
-app.listen(PORT, () => console.log('work'));
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
