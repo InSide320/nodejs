26ccf4b590667dee1d2b89db1d286fc5602d412e
Denys Kud <dekud2109@gmail.com>
2025-07-16 11:53:28 +0300

    Added method-override support and enhance article form with tags functionality

--- 10-crud-operation/implementation/app.js
+++ 10-crud-operation/implementation/app.js
@@ -5,8 +5,9 @@ const session = require('express-session');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const sessionMiddleware = require('./db/session');
-
-const { connectDB } = require('./db/db');
+const methodOverride = require('method-override');
+const {connectDB} = require('./db/db');
+const {stringifyTags} = require("./utils/tags");
 
 const app = express();
 
@@ -14,29 +15,30 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', './views');
 app.use(express.static(`${__dirname}/assets`));
-app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(sessionMiddleware);
 
 app.use(morgan('tiny', {
-  skip: req => req.url.startsWith('/.well-known')
+    skip: req => req.url.startsWith('/.well-known')
 }));
+app.use(methodOverride('_method'));
 
 app.use((req, res, next) => {
-  app.locals.user = req.session?.user || null;
-  app.locals.email = req.session?.email || null;
-  next();
+    app.locals.user = req.session?.user || null;
+    app.locals.email = req.session?.email || null;
+    res.locals.stringifyTags = stringifyTags;
+    next();
 });
 
-connectDB().then(({ usersCollection, articlesCollection }) => {
-  // pseudo  Immediately Invoked Function Expression (IIFE)
-  app.use('/', require('./routes/index')({ articlesCollection }));
-  app.use('/', require('./routes/auth')({ usersCollection }));
-  app.use('/', require('./routes/dashboard')({ usersCollection }));
-  app.use('/articles', require('./routes/articles')({ articlesCollection}));
+connectDB().then(({usersCollection, articlesCollection}) => {
+    // pseudo  Immediately Invoked Function Expression (IIFE)
+    app.use('/', require('./routes/index')({articlesCollection}));
+    app.use('/', require('./routes/auth')({usersCollection}));
+    app.use('/', require('./routes/dashboard')({usersCollection}));
+    app.use('/articles', require('./routes/articles')({articlesCollection}));
 
-  app.listen(config.port, () => {
-    console.log('Сервер запущено на http://localhost:' + config.port);
-  });
+    app.listen(config.port, () => {
+        console.log('Сервер запущено на http://localhost:' + config.port);
+    });
 });
