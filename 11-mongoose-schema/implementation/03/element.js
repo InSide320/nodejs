1bbdc734606fb18d7df70b2194e9234c539859f7
Denys Kud <dekud2109@gmail.com>
2025-07-19 09:04:29 +0300

    Changed validation to birthDate field

--- 11-mongoose-schema/implementation/03/element.js
+++ 11-mongoose-schema/implementation/03/element.js
@@ -9,7 +9,12 @@ const elementSchema = new mongoose.Schema({
     },
     birthDate: {
         type: String,
-        set: (date) => (/^\d{4}-\d{2}-\d{2}$/).test(date) ? date : undefined,
+        validate: {
+            validator: function (v) {
+                return /^\d{4}-\d{2}-\d{2}$/.test(v);
+            },
+            message: props => `${props.value} дата має бути у форматі YYYY-MM-DD!`
+        },
     },
     email: {
         type: String
